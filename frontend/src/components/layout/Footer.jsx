8b9f19c31f8f13f9c98f5e92a96084db12f2abf6
Bozhidar Yordanov <108609381+BozhidarYordanov54@users.noreply.github.com>
2025-03-14 23:42:56 +0200

    Add Footer component with social links and newsletter subscription; create forms.css for styling forms

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,8 +1,57 @@
-export default function Footer(){
-    return(
+import { NavLink } from "react-router"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSquareCheck, faMailBulk } from "@fortawesome/free-solid-svg-icons"
+import { faInstagram, faFacebook, faTiktok, faXTwitter } from "@fortawesome/free-brands-svg-icons";
+
+const socials = [
+    { key: 1, path: "https://www.instagram.com", icon: <FontAwesomeIcon icon={faInstagram} /> },
+    { key: 2, path: "https://www.facebook.com", icon: <FontAwesomeIcon icon={faFacebook} /> },
+    { key: 3, path: "https://www.tiktok.com", icon: <FontAwesomeIcon icon={faTiktok} /> },
+    { key: 4, path: "https://www.twitter.com", icon: <FontAwesomeIcon icon={faXTwitter} /> },
+];
+
+export default function Footer() {
+    return (
         <>
             <footer className="site-footer">
-
+                <div className="site-info">
+                    <div className="socials-container">
+                        <div className="site logo">
+                            <NavLink to="/">
+                                <div className="logo">
+                                    <FontAwesomeIcon icon={faSquareCheck} />
+                                </div>
+                                <p className="brand-name">TooDoo</p>
+                            </NavLink>
+                        </div>
+                        <div className="socials">
+                            {socials.map((social) => {
+                                return (
+                                    <NavLink to={social.path} key={social.key}>
+                                        {social.icon}
+                                    </NavLink>
+                                )
+                            })}
+                        </div>
+                    </div>
+                    <div className="site-links">
+                        <NavLink to="/about">About</NavLink>
+                        <NavLink to="/contact">Contact</NavLink>
+                        <NavLink to="/privacy">Privacy</NavLink>
+                    </div>
+                </div>
+                <div className="newsletter-wrapper">
+                    <FontAwesomeIcon icon={faMailBulk} />
+                    <h2>Subscribe to our newsletter</h2>
+                    <p>Stay up to date with our newest futures and plans</p>
+                    <div className="newsletter-form">
+                        <input type="email" placeholder="Enter your email" />
+                        <button type="submit">Subscribe</button>
+                    </div>
+                </div>
+                <div className="copyright-wrapper">
+                    <p>&copy; {new Date().getFullYear()} TooDoo. All rights reserved.</p>
+                </div>
             </footer>
         </>
     )
