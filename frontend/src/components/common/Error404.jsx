d074142908a3fa8a33902d13f6adc6086b2f23af
Bozhidar Yordanov <108609381+BozhidarYordanov54@users.noreply.github.com>
2025-03-28 17:06:24 +0200

    Enhance 404 error page; improve layout and styling, add navigation link and image

--- frontend/src/components/common/Error404.jsx
+++ frontend/src/components/common/Error404.jsx
@@ -1,8 +1,19 @@
+import { NavLink } from "react-router-dom";
+
+import "../../css/error-page.css";
+
 export default function Error404() {
     return (
-        <div>
-            <h1>404</h1>
-            <p>Page not found</p>
+        <div className="error-container">
+            <div className="error-content">
+                <h1 className="error-title">404</h1>
+                <p className="error-subtitle">Page Not Found</p>
+                <p className="error-text">Ooops, it's a dead end...</p>
+                <NavLink to="/" className="error-button">Back to Home</NavLink>
+            </div>
+            <div className="error-image">
+                <img src="../../assets/astronaut.png" alt="Floating astronaut" />
+            </div>
         </div>
     );
 }
